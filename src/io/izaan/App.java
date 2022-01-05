ee8e88e338b5219aa3bb12822f6442ad797ad39c
Abu Sufian <abusufian145@gmail.com>
2022-01-05 16:50:56 -0500

    io/. file added

---
+++ src/io/izaan/App.java
@@ -0,0 +1,11 @@
+package io.izaan;
+
+public class App {
+    public static void main(String[] args) {
+
+        Atm atm = new Atm();
+        atm.getAtmId();
+        Atm.abcdBank.getTotalCashCountOnAtm();
+
+    }
+}
---
+++ src/io/izaan/VisaCard.java
@@ -0,0 +1,7 @@
+package io.izaan;
+
+public class VisaCard {
+
+
+
+}
