143a030ac96a39e205c04f73a97d0775c1792049
Abu Sufian <abusufian145@gmail.com>
2022-01-10 16:50:43 -0500

    update to Math5 class

--- src/com/company/Math5.java
+++ src/com/company/Math5.java
@@ -19,11 +19,33 @@ public class Math5 {
         switch (operator) {
             case '+':
             addition(num1,num2);
+            break;
+            case '*':
+            multiplication(num1,num2);
+            case '-':
+                subtraction(num1,num2);
+            case'/':
+                division(num1,num2);
+             break;
+            default:
+                System.out.println("invalid request");
+
         }
     }
     public static void addition (int num1, int num2){
         int result = num1+num2;
         System.out.println("both number add to : " + result);
     }
-
+public static void multiplication (int num1, int num2){
+        int result = num1*num2;
+    System.out.println("multiplies to: " + result);
+}
+public static void subtraction (int num1, int num2){
+        int result = num1-num2;
+    System.out.println("difference is: " + result);
+}
+public static void division (int num1, int num2){
+        int result = num1/num2;
+    System.out.println("equals : " + result);
+}
 }
