8d3f848cf0d42c97623bce67afd240524f316d3d
Abu Sufian <abusufian145@gmail.com>
2022-01-12 16:50:51 -0500

    adding WhileLoopDemo5

---
+++ src/com/company/WhileLoopDemo5.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class WhileLoopDemo5 {
+
+
+    public static void main(String[] args) {
+
+        String name = "Rafi";
+        int age = 26;
+    if (name.equals("Rafi")){
+        System.out.println("Hello Rafi");
+    }while (age <=35){
+            System.out.println("Age: " +age+ ", Time is flying make the most out of it; you " +
+                    "have just certain number of years till its too late");
+    age= age+1;
+
+    }
+    }
+}
